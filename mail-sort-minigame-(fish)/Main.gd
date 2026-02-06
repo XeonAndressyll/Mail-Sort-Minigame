4e0fe47f64868eb8c3b59e4ff12b60424fa24ad0
XeonAndressyll <85351240+XeonAndressyll@users.noreply.github.com>
2026-02-05 21:23:19 -0500

    NewGamebutton
    put a button in and added score

--- mail-sort-minigame-(fish)/Main.gd
+++ mail-sort-minigame-(fish)/Main.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 @onready var button : Button = $Button
 @onready var button2 : Button = $Button2
+@onready var NewGameButton : Button = $NewGameButton
+@onready var target : Label = $TargetIcon
+
 var score : int = 0
 
-
-@onready var target : Label = $TargetIcon
 
 var FishData_scene: PackedScene = load("res://fish_data.tscn")
 var FishData: Node = FishData_scene.instantiate()
@@ -21,6 +22,9 @@ var file_location : String = "res://Fish-type-mailslot.txt"
 func _ready() -> void:
 	button.pressed.connect(button_press.bind(button))
 	button2.pressed.connect(button_press.bind(button2))
+	NewGameButton.pressed.connect(reset)
+	
+	
 	
 	add_child(FishData)
 	FishData.owner = self
@@ -30,10 +34,8 @@ func _ready() -> void:
 	_FishButton.position = Vector2(200, 200)
 	_FishButton.scale = Vector2(8, 8)
 	
-	$AcceptDialog.confirmed.connect(reset)
-
 	fish_get_data()
-
+	
 	target_pool = $FishData.get_meta("TotalFishArray")
 	
 	print("What node.2d thinks TotalFishArray is: ", $FishData.get_meta("TotalFishArray"))
@@ -54,7 +56,7 @@ func fish_get_data() -> void:
 	print(_array)
 
 
-func reset() -> void: 
+func NewTarget() -> void: 
 	target.text = target_pool.pick_random()
 	
 	var target_wrongpool : Array= []
@@ -64,6 +66,7 @@ func reset() -> void:
 			pass
 		else:
 			target_wrongpool.append(item)
+	
 	
 	var button_pool : Array= [button, button2]
 	
@@ -76,27 +79,24 @@ func reset() -> void:
 		if correct_button == i:
 			button_pool[i].set_meta("correct_button", true)
 			button_pool[i].text = target.text
-			
+		
+		
 		else:
 			button_pool[i].set_meta("correct_button", false)
 			print(button_pool[i])
 			print(button_pool[i].get_meta("correct_button"))
 			button_pool[i].text = target_wrongpool.pick_random()
 		print(button_pool[i])
-	
+
+
 func button_press(pressed_button : Button) -> void :
 	if pressed_button.get_meta("correct_button"):
-		$AcceptDialog.dialog_text = "Gud"
+		NewTarget()
 		print(pressed_button.get_meta("correct_button"))
 		scoreup()
 	else:
-		$AcceptDialog.dialog_text = "Uh oh"
-		print(pressed_button.get_meta("correct button"))
-		
-
-	$AcceptDialog.popup_centered()
-	
-
+		NewTarget()
+		print(pressed_button.get_meta("correct button"))	
 	print("It got pressed")
 	
 
@@ -107,6 +107,12 @@ func scoreup() -> void:
 	$UserInterface/Score.set_text("Score: " + str(score))
 	print(score)
 	print($UserInterface/Score.get_text())
+
+func reset() -> void:
+	score = 0
+	$UserInterface/Score.set_text("Score: " + str(score))
+	print($UserInterface/Score.get_text())
+	NewTarget()
 
 
 
